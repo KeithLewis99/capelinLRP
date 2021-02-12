4043d311dbc75efa377e006369c2148b9d1f8f7e
Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2021-02-12 20:12:02 -0330

    Minor change: made all ggplot figures into plotly figures and soft coded some variables.

--- setUpProject_testOnly.R
+++ setUpProject_testOnly.R
@@ -13,6 +13,7 @@ rm(list=ls())
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
 #put this file in the folder where you want to create a subfolder containing the project
 if(!dir.exists("data"))dir.create("data")
@@ -47,13 +48,19 @@ abline(v = 18.5)
 abline(v = 9.8)
 
 #Make above plot nice
-p <- ggplot(ld, aes(x = rank, y = avg_density, colour = year))
+p <- ggplot(ld, aes(x = rank, y = avg_density, colour = year, text = paste(
+  "Year: ", year, "\n",
+  "Density: ", avg_density, "\n",
+  "Rank: ", rank,
+  sep = ""
+)))
 p <- p + geom_point()
 p <- p + scale_colour_continuous(type = "viridis")
 #p <- p + scale_colour_gradient(low = "yellow", high = "darkgreen")
 p <- p + theme_bw()
 p
 
+ggplotly(p, tooltip = "text")
 ggsave("figs/1-larvae-density-rank.pdf")
 
 ## read in capelin data----
@@ -83,13 +90,19 @@ plot(cap$rank, cap$abundance_med)
 abline(v = 22.5) #everything to the right is pre1991
 
 #pretty graph
-p <- ggplot(cap, aes(x = rank, y = abundance_med, colour = year))
+p <- ggplot(cap, aes(x = rank, y = abundance_med, colour = year, text = paste(
+  "Year: ", year, "\n",
+  "Abundance: ", abundance_med, "\n",
+  "Rank: ", rank,
+  sep = ""
+ )))
 p <- p + geom_point()
 p <- p + scale_colour_continuous(type = "viridis")
 #p <- p + scale_colour_gradient(low = "yellow", high = "darkgreen")
 p <- p + theme_bw()
 p
 
+ggplotly(p, tooltip = "text")
 ggsave("figs/2-Abundance-rank-year.pdf")
 
 ##First stab at a S-R relationship----
@@ -132,7 +145,52 @@ quantile(cap$anomaly, c(0.1, 0.9), na.rm = T)
 plot(cap$year, cap$anomaly)
 abline(h = 1.56)
 
-#post collapse
+
+# Wheeland/Haddock type plot Fig. 7
+p <- ggplot(cap, aes(x = year, y = anomaly, text = paste(
+  "Year: ", year, "\n",
+  "abund: ", abundance_med, "\n",
+  "Anomaly: ", anomaly, "\n",
+  "Rank: ", rank,
+  sep = ""
+)))
+p <- p + geom_bar(stat = "identity")
+p <- p + geom_hline(yintercept = 1.61)
+p <- p + xlab("Year") 
+p <- p + ylab("Recruitment anomolies")
+p <- p + theme_bw()
+p
+
+ggplotly(p, tooltip = "text")
+ggsave("figs/3-Biomass_all-year-anomaly.pdf")
+
+
+#get the value for the lowest index to generate large recruitment (or a large index)
+biomass90 <- quantile(cap, c(0.1, 0.9), na.rm = T)[2]
+y <- subset(cap, biomass_med_lead >= biomass90, na.rm = T)
+z <- min(y$biomass_med)
+
+
+# Wheeland/Haddock type plot Fig. 8
+p <- ggplot(cap, aes(x = biomass_med, y = biomass_med_lead, text = paste(
+  "Year: ", year, "\n",
+  "Recruitment: ", biomass_med_lead, "\n",
+  "Biomass: ", biomass_med, "\n",
+  "Rank: ", rank,
+  sep = ""
+)))
+p <- p + geom_point()
+p <- p + geom_vline(xintercept = z) #document where this came from - soft code it
+p <- p + xlab("Index (ktonnes)") 
+p <- p + ylab("Recruitment (ktonnes)")
+p <- p + theme_bw()
+p
+
+ggplotly(p, tooltip = "text")
+ggsave("figs/4-Biomass_all-index-recruit.pdf")
+
+
+##post collapse----
 #calculate anomalies - get mean and SD
 capMeanPost <- mean(cap_postCollapse$biomass_med_lead, na.rm = T)
 capSDPost <- sd(cap_postCollapse$biomass_med_lead, na.rm = T)
@@ -149,25 +207,39 @@ abline(h = 1.61)
 #View(cap_postCollapse)
 
 # Wheeland/Haddock type plot Fig. 7
-p <- ggplot(cap_postCollapse, aes(x = year, y = anomaly))
+p <- ggplot(cap_postCollapse, aes(x = year, y = anomaly, text = paste(
+  "Year: ", year, "\n",
+  "abund: ", abundance_med, "\n",
+  "Anomaly: ", anomaly, "\n",
+  "Rank: ", rank,
+  sep = ""
+)))
 p <- p + geom_bar(stat = "identity")
 p <- p + geom_hline(yintercept = 1.61)
 p <- p + xlab("Year") 
 p <- p + ylab("Recruitment anomolies")
 p <- p + theme_bw()
 p
-ggsave("figs/3-Biomass-year-anomaly.pdf")
+
+ggplotly(p, tooltip = "text")
+ggsave("figs/3-Biomass_postCollapse-year-anomaly.pdf")
 
 plot(cap_postCollapse$year, cap_postCollapse$biomass_med_lead)
 plot(cap_postCollapse$biomass_med, cap_postCollapse$biomass_med_lead)
 
-
-  y <- subset(cap_postCollapse, biomass_med_lead >= biomass90, na.rm = T)
-  z <- min(y$biomass_med)
+#get the value for the lowest index to generate large recruitment (or a large index)
+y <- subset(cap_postCollapse, biomass_med_lead >= biomass90, na.rm = T)
+z <- min(y$biomass_med)
 
 
 # Wheeland/Haddock type plot Fig. 8
-p <- ggplot(cap_postCollapse, aes(x = biomass_med, y = biomass_med_lead))
+p <- ggplot(cap_postCollapse, aes(x = biomass_med, y = biomass_med_lead, text = paste(
+  "Year: ", year, "\n",
+  "Recruitment: ", biomass_med_lead, "\n",
+  "Biomass: ", biomass_med, "\n",
+  "Rank: ", rank,
+  sep = ""
+)))
 p <- p + geom_point()
 p <- p + geom_vline(xintercept = z) #document where this came from - soft code it
 p <- p + xlab("Index (ktonnes)") 
@@ -175,5 +247,7 @@ p <- p + ylab("Recruitment (ktonnes)")
 p <- p + theme_bw()
 p
 
-ggsave("figs/4-Biomass-index-recruit.pdf")
+ggplotly(p, tooltip = "text")
+ggsave("figs/4-Biomass_postCollapse-index-recruit.pdf")
+
 #test

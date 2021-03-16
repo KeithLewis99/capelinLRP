6c56c60c7677831bc9eb914d02faaef8e043038e
Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2021-03-16 09:44:58 -0230

    Massive change: moved all the figures to simpleLRP_figs.R

--- simpleLRP_dat.R
+++ simpleLRP_dat.R
@@ -53,29 +53,7 @@ ld$rank <- rank(ld$avg_density)
 arrange(ld, rank)
 ld$avg_density_lag2 <- lag(ld$avg_density, 2)
 
-## Plots of larval density----
-#basic plot of year v density
-plot(ld$year, ld$avg_density)
 
-#basic plot of rank v. density with 10, 50, and 90th percentiles
-plot(ld$rank, ld$avg_density)
-abline(v=2.8)
-abline(v = 18.5)
-abline(v = 9.8)
-
-#pretty plot of rank v. density
-LD_rank <- Scatter1(df = ld, xaxis = rank, yaxis = avg_density, colour = year, 
-                    c1 = "Year: ", c2 = "Rank: ", c3 = "Density: ",                     
-                    xlab = "Rank", ylab = "Larval Density (#/m^-3)", 
-                    filename = "figs/1-larvae-density-rank.pdf", save = save)
-LD_rank
-
-
-
-ld_YEAR <- Scatter1(df = ld, xaxis = year, yaxis = avg_density, colour = rank, 
-                      c1 = "Rank: ", c2 = "Year: ", c3 = "tice: ", xlab = "Year", 
-                      ylab = "Larval Density (#/m^-3)",
-                      filename = "figs/2-cond-year-rank.pdf", save = save)
 
 
 ## read in capelin data----
@@ -96,28 +74,6 @@ cap$abundance_med_lag2 <- lag(cap$abundance_med, 2)
 #View(cap)
 
 
-## Plots of capelin abundance----
-#plot biomass and abundance
-plot(cap$abundance_med, cap$biomass_med)
-
-#basic plot of year v capelin abundance
-plot(cap$year, cap$abundance_med)
-
-#basic plot of rank v capelin abundance
-plot(cap$rank, cap$abundance_med)
-abline(v = 22.5) #everything to the right is pre1991
-
-#pretty plot of rank v capelin abundance
-cap_rank <- Scatter1(df = cap, xaxis = rank, yaxis = abundance_med, colour = year, 
-                     c1 = "Year: ", c2 = "Rank: ", c3 = "Abundance: ", xlab = "Rank", 
-                     ylab = "Capelin abundance (millions?)",
-                     filename = "figs/2-Abundance-rank-year.pdf", save = save)
-cap_rank
-
-cap_Year <- Scatter1(df = cap, xaxis = year, yaxis = abundance_med, colour = rank, 
-                      c1 = "Rank: ", c2 = "Year: ", c3 = "Abundance: ", 
-                     xlab = "Year", ylab = "Capelin abundance (millions?)",
-                      filename = "figs/2-cond-rank-year.pdf", save = save)
 
 
 ## read in ice data----
@@ -130,14 +86,7 @@ str(ice)
 #create a rank colum
 ice$rank <- rank(ice$tice)
 
-tice_rank <- Scatter1(df = ice, xaxis = rank, yaxis = tice, colour = year, 
-                     c1 = "Year: ", c2 = "Rank: ", c3 = "tice: ", 
-                     xlab = "Rank", ylab = "Ice retreat (tice - DOY)",
-                     filename = "figs/2-tice-rank-year.pdf", save = save)
-tice_YEAR <- Scatter1(df = ice, xaxis = year, yaxis = tice, colour = rank, 
-                      c1 = "Rank: ", c2 = "Year: ", c3 = "tice: ", 
-                      xlab = "Year", ylab = "Ice retreat (tice - DOY)",
-                      filename = "figs/2-cond-year-rank.pdf", save = save)
+
 
 
 ## read in condition data----
@@ -150,13 +99,7 @@ str(cond)
 cond$rank <- rank(cond$meanCond)
 cond$cond_lag1 <- lag(cond$meanCond)
 
-cond_rank <- Scatter1(df = cond, xaxis = rank, yaxis = meanCond, colour = year, 
-                      c1 = "Year: ", c2 = "Rank: ", c3 = "Cond: ", xlab = "Rank", ylab = "Condition ()",
-                      filename = "figs/2-cond-rank-year.pdf", save = save)
 
-cond_YEAR <- Scatter1(df = cond, xaxis = year, yaxis = meanCond, colour = rank, 
-                      c1 = "Rank: ", c2 = "Year: ", c3 = "Cond: ", xlab = "Year", ylab = "Condition ()",
-                      filename = "figs/2-cond-year-rank.pdf", save = save)
 
 ## read in maturity data----
 #read and check data
@@ -170,18 +113,6 @@ matA$rank <- rank(matA$age2)
 matA$mat2_lag1 <- lag(matA$age2, 1)
 
 
-mat2_rank <- Scatter1(df = matA, xaxis = rank, yaxis = age2, colour = year, 
-                      c1 = "Year: ", c2 = "Rank: ", c3 = "%Age_2: ", xlab = "Rank", ylab = "Capelin Age 2 (%)",
-                      filename = "figs/2-mat2-rank-year.pdf", save = save)
-
-mat2_YEAR <- Scatter1(df = matA, xaxis = year, yaxis = age2, colour = rank, 
-                      c1 = "Rank: ", c2 = "Year: ", c3 = "%Age_2: ", xlab = "Year", ylab = "Capelin Age 2 (%)",
-                      filename = "figs/2-mat2-year-rank.pdf", save = save)
-
-
-mat3_YEAR <- Scatter1(df = matA, xaxis = year, yaxis = age3, colour = rank, 
-                      c1 = "Rank: ", c2 = "Year: ", c3 = "%Age_3: ", xlab = "Year", ylab = "Capelin Age 3 (%)",
-                      filename = "figs/2-mat3-year-rank.pdf", save = save)
 
 
 ## #read in age disaggregated data----
@@ -203,9 +134,10 @@ str(ageD)
 
 #manipulate 
 
+
 #make variables
-matA$rank <- rank(matA$age2)
-matA$mat2_lag1 <- lag(matA$age2, 1)
+#ageD$rank <- rank(ageD$age2)
+#matA$mat2_lag1 <- lag(matA$age2, 1)
 
 
 ## join all dataframes with lags----
@@ -216,7 +148,6 @@ df_lag <- ls %>% reduce(left_join, by ="year") %>%
   select(year, abundance_med, avg_density_lag2, tice, cond_lag1, age2, mat2_lag1)
 
 
-fig <- plot_ly(data = df_lag, x = ~year, y = ~abundance_med)
 
 # Create indicator (index) of abundance next year based on % of immature Age 2 (1-age 2) that are will mature the next year.
 df_lag$abund_age3 <- "NA"
@@ -302,6 +233,7 @@ temp5 <- df_lag %>%
 
 ggplotly(temp5)
 
+
 ## S-R relationship----
 # NOTE THAT THE S-R RELATIONSHIPS ARE IN THE BELOW AND IN THE DASHBOARD
 
@@ -326,7 +258,7 @@ quantile(cap_postCollapse$biomass_med, c(0.1, 0.9), na.rm = T)
 quantile(cap_postCollapse$biomass_med_lead, c(0.1, 0.9), na.rm = T)
 #assuiming that i've done this right, not much here - 
 
-# NOTE THAT THE S-R RELATIONSHIPS ARE IN THE BELOW AND IN THE 
+# NOTE THAT THE S-R RELATIONSHIPS ARE IN THE BELOW AND IN THE DASHBOARD
 
 
 
@@ -336,36 +268,24 @@ quantile(cap_postCollapse$biomass_med_lead, c(0.1, 0.9), na.rm = T)
 # calculate anomalies - get mean and SD
 cap <- anomaly(cap, "biomass_med_lead")
 
+# simple test plot
+plot(cap$year, cap$anomaly)
+abline(h = h90)
 
 # get the quantile and extract the 90th for the hline
 h90 <- quantile(cap$anomaly, c(0.1, 0.9), na.rm = T)[2]
 # confirm above value and check on plot
 quantile(cap$anomaly, c(0.1, 0.9), na.rm = T)
 
-# simple test plot
-plot(cap$year, cap$anomaly)
-abline(h = h90)
-
-
-# Wheeland/Haddock type plot Fig. 7
-Anomaly_year_all <- Bar1(df = cap, xaxis = year, yaxis = anomaly, c2 = "Abundance: ", c3 = "Anomaly: ", xlab = "Year", ylab = "Recruitment anomolies", hline = h90, filename = "figs/3-Biomass_all-year-anomaly.pdf", save = save)
-Anomaly_year_all
-
-
-
-#get the value for the lowest index to generate large recruitment (or a large index)
-biomass90_1 <- quantile(cap$biomass_med, c(0.1, 0.9), na.rm = T)[2]
-y1 <- subset(cap, biomass_med_lead >= biomass90_1, na.rm = T)
-v90 <- min(y1$biomass_med)
-
 
 # simple test plot
 plot(cap$biomass_med, cap$biomass_med_lead)
 abline(v = v90)
 
-# Wheeland/Haddock type plot Fig. 8
-SR_all <- Scatter2(df = cap, xaxis = biomass_med, yaxis = biomass_med_lead, c2 = "Biomass: ", c3 = "Recruitment: ", xlab = "Index (ktonnes)", ylab = "Recruitment (ktonnes)", vline = v90, filename = "figs/4-Biomass_all-index-recruit.pdf", save = save)
-SR_all
+#get the value for the lowest index to generate large recruitment (or a large index)
+biomass90_1 <- quantile(cap$biomass_med, c(0.1, 0.9), na.rm = T)[2]
+y1 <- subset(cap, biomass_med_lead >= biomass90_1, na.rm = T)
+v90 <- min(y1$biomass_med)
 
 
 
@@ -385,12 +305,6 @@ plot(cap_postCollapse$year, cap_postCollapse$anomaly)
 abline(h = h90_post)
 
 
-# Wheeland/Haddock type plot Fig. 7
-
-Anomaly_year_post <- Bar1(df = cap_postCollapse, xaxis = year, yaxis = anomaly, c2 = "Abundance: ", c3 = "Anomaly: ", xlab = "Year", ylab = "Recruitment anomolies", hline = h90, filename = "figs/5-Biomass_postCollapse-year-anomaly.pdf", save = save)
-Anomaly_year_post
-
-
 #get the value for the lowest index to generate large recruitment (or a large index)
 biomass90_1 <- quantile(cap_postCollapse$biomass_med, c(0.1, 0.9), na.rm = T)[2]
 y1 <- subset(cap_postCollapse, biomass_med_lead >= biomass90_1, na.rm = T)
@@ -401,8 +315,4 @@ v90_post <- min(y1$biomass_med)
 plot(cap_postCollapse$year, cap_postCollapse$biomass_med_lead)
 plot(cap_postCollapse$biomass_med, cap_postCollapse$biomass_med_lead)
 
-
-# Wheeland/Haddock type plot Fig. 8
-SR_all <- Scatter2(df = cap_postCollapse, xaxis = biomass_med, yaxis = biomass_med_lead, c2 = "Biomass: ", c3 = "Recruitment: ", xlab = "Index (ktonnes)", ylab = "Recruitment (ktonnes)", vline = v90, filename = "figs/6-Biomass_all-index-recruit.pdf", save = save)
-SR_all
 

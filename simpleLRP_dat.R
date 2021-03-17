136e2b259bb6c2526934b0ca0d417305a91310e8
Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2021-03-17 16:08:21 -0230

    Medium change: cleaned up a lot of variable names and did a lot of annotation.

--- simpleLRP_dat.R
+++ simpleLRP_dat.R
@@ -51,7 +51,7 @@ quantile(ld$avg_density, c(0.1, 0.9))
 #create a rank column
 ld$rank <- rank(ld$avg_density)
 arrange(ld, rank)
-ld$avg_density_lag2 <- lag(ld$avg_density, 2)
+ld$avg_densityt_2 <- lag(ld$avg_density, 2)
 
 #basic plot of year v density
 plot(ld$year, ld$avg_density)
@@ -114,7 +114,7 @@ cond <- read_csv("data/condition_ag1_2_MF_out.csv", col_types = cols(
 str(cond)
 
 cond$rank <- rank(cond$meanCond)
-cond$cond_lag1 <- lag(cond$meanCond)
+cond$cond_lagt_1 <- lag(cond$meanCond)
 
 
 
@@ -127,7 +127,7 @@ str(matA)
 
 #make variables
 matA$rank <- rank(matA$age2)
-matA$mat2_lag1 <- lag(matA$age2, 1)
+matA$mat2t_1 <- lag(matA$age2, 1)
 
 
 
@@ -164,95 +164,96 @@ str(ageD)
 
 ls <- list(cap, ld, ice, cond, matA)
 df_lag <- ls %>% reduce(left_join, by ="year") %>%
-  select(year, abundance_med, avg_density_lag2, tice, cond_lag1, age2, mat2_lag1)
+  select(year, abundance_med, avg_densityt_2, tice, cond_lagt_1, age2, mat2t_1)
+str(df_lag)
 
 
+# Create indicator (index) of abundance next year based on % of immature Age 2 (1-age 2) that are will mature the next year. This is probably not quite right as the AGe 1's are still in but it assumes that the Age 3+ add very little.
+df_lag$abundAge3_t <- "NA"
 
-# Create indicator (index) of abundance next year based on % of immature Age 2 (1-age 2) that are will mature the next year.
-df_lag$abund_age3 <- "NA"
+df_lag$abundAge3_t <- ((1-(df_lag$mat2t_1*0.01))*lag(df_lag$abundance_med, 1))
+plot(df_lag$mat2t_1, df_lag$abundance_med)
+plot(df_lag$abundAge3_t, df_lag$abundance_med)
 
-df_lag$abund_age3 <- ((1-(df_lag$mat2_lag1*0.01))*lag(df_lag$abundance_med, 1))
-plot(df_lag$mat2_lag1, df_lag$abundance_med)
-plot(df_lag$abund_age3, df_lag$abundance_med)
-
-abund_age3_YEAR <- Scatter1(df = df_lag, xaxis = year, yaxis = abund_age3, colour = NULL, 
+abund_age3_YEAR <- Scatter1(df = df_lag, xaxis = year, yaxis = abundAge3_t, colour = NULL, 
                       c1 = "Rank: ", c2 = "Year: ", c3 = "Abund_Age_3: ", xlab = "Year", ylab = "Abundance Age 3",
                       filename = "figs/2-abund3-year.pdf", save = save)
 
-df_lag$abundance_med_t2 <- lag(df_lag$abundance_med,2)
 
-# this is the strength of the AGe 3 cohort; lagged two years so that it can be compared to the index for a S-R relationship
-df_lag$SRt3 <- lag(df_lag$abundance_med*(1-(df_lag$age2*0.01)), 3)
+# This is the abundance at t-2 - useful for a crude S-R relationship
+df_lag$abundance_med_t_2 <- lag(df_lag$abundance_med,2)
+
+# this is the strength of the AGe 3 cohort; lagged three years so that it can be compared to the index for a S-R relationship
+df_lag$SR_age3_t3 <- lag(df_lag$abundance_med*(1-(df_lag$age2*0.01)), 3)
 
 # relationship between immature at age 2 that will influence recruits in 3 years and mature at age 2 that will influence recruits in 2 years
-df_lag$immat_lag3 <- "NA"
-df_lag$immat_lag3 <- lag((100-df_lag$age2), 3)
-plot(df_lag$immat_lag3[c(7:34)], df_lag$abundance_med[c(7:34)])
-summary(lm(df_lag$abundance_med ~ df_lag$immat_lag3 + df_lag$mat2_lag1))
+df_lag$immat_t3 <- "NA"
+df_lag$immat_t3 <- lag((100-df_lag$age2), 3)
+plot(df_lag$immat_t3[c(7:34)], df_lag$abundance_med[c(7:34)])
+summary(lm(df_lag$abundance_med ~ df_lag$immat_t3 + df_lag$mat2t_1))
 
-#this is the strength of the Age 2 cohort, lagged one year
-df_lag$SRt2 <- lag(df_lag$abundance_med*(df_lag$age2*0.01), 2)
+#this is the strength of the Age 2 cohort, lagged one year - fish that will be Age 3 the next year
+df_lag$SRage3_t1 <- lag(df_lag$abundance_med*(df_lag$age2*0.01), 1)
 
+## explore lagged relationships between %Mat@AG2 and abundance----
+# Influence of abundance (t) on Abundance of age 3 (t3) 
 temp1a <- df_lag %>%
   filter(year > 1992) %>%
-  ggplot(aes(abundance_med, SRt3, text = paste("Year", year))) + geom_point()
+  ggplot(aes(abundance_med, SR_age3_t3, text = paste("Year", year))) + geom_point()
   
   ggplotly(temp1a)
 
-# abundance med = recruitment here
-temp1b <- df_lag %>%
-    filter(year > 1992) %>%
-    ggplot(aes(SRt2, abundance_med,  text = paste("Year", year))) + geom_point()
+# reverse of the above graph - not sure if this has any value
+#temp1b <- df_lag %>%
+#    filter(year > 1992) %>%
+ #   ggplot(aes(SR_age3_t3, abundance_med,  text = paste#("Year", year))) + geom_point()
   
-  ggplotly(temp1b)
+ # ggplotly(temp1b)
   
-  
+# Influence of the larvae (t-2) on Age3 (t3): this is a bit of a stretch.  Good correlation between ld and abundance_med but this is taking it three years beyond that  
 temp1c <- df_lag %>%
     filter(year > 1992) %>%
-    ggplot(aes(SRt2, avg_density_lag2,  text = paste("Year", year))) + geom_point()
+    ggplot(aes(SR_age3_t3, avg_densityt_2,  text = paste("Year", year))) + geom_point()
   ggplotly(temp1c)
 
   
-  
+# no idea why this is here  
 temp1d <- df_lag %>%
     filter(year > 1992) %>%
-    ggplot(aes(SRt3, lag(avg_density_lag2, 1),  text = paste("Year", year))) + geom_point()
+    ggplot(aes(SR_age3_t3, lag(avg_densityt_2, 1),  text = paste("Year", year))) + geom_point()
   ggplotly(temp1d)
-
   
-  
-    
-# just the 3 "recovery years"
-temp2 <- df_lag %>%
-    filter(year >= 2011 & year <=2015)
 
-
-# tiered approach  
+## tiered approach----  
+# start with the relationship between LD (t-2) and capelin abundance
 temp3 <- df_lag %>%
   filter(year > 1990) %>%
-  ggplot(aes(avg_density_lag2, abundance_med, text = paste("Year", year))) + geom_point()
+  ggplot(aes(avg_densityt_2, abundance_med, text = paste("Year", year))) + geom_point()
 
 ggplotly(temp3)
 
 
-
+# Then, based on the idea that only LD values > 2000 have led to good recruitment, lets see if any thresholds in tice: filter the data accordingly
 temp4 <- df_lag %>%
-  filter(year > 1990 & avg_density_lag2 < 2000) %>%
+  filter(year > 1990 & avg_densityt_2 > 2000) %>%
   ggplot(aes(tice, abundance_med, text = paste(
-    "Year", year, "\n", "Density", avg_density_lag2, "\n",  
+    "Year", year, "\n", "Density", avg_densityt_2, "\n",  
                                                sep = ""))) + geom_point()
 
 ggplotly(temp4)
 
-
+# BAsed on the logic that late tice > 80 days, and only <80 days has produced the "large" numbers
 temp5 <- df_lag %>%
-  filter(year > 1990 & avg_density_lag2 < 2000) %>%
-  ggplot(aes(cond_lag1, abundance_med, text = paste(
-    "Year", year, "\n", "Cond", cond_lag1, "\n",  
+  filter(year > 1990 & avg_densityt_2 > 2000) %>%
+  ggplot(aes(cond_lagt_1, abundance_med, text = paste(
+    "Year", year, "\n", "Cond", cond_lagt_1, "\n",  
     sep = ""))) + geom_point()
 
 ggplotly(temp5)
 
+#Suggests that condition > 0.99 might lead to large abundance.
+
+# the problem with this approach is the small sample size and uncertainty, i.e., driven by 2013-2015 and that some low abundance year had good condition and "good" tice.
 
 ## S-R relationship----
 # NOTE THAT THE S-R RELATIONSHIPS ARE IN THE BELOW AND IN THE DASHBOARD
@@ -339,3 +340,4 @@ plot(cap_postCollapse$biomass_med, cap_postCollapse$biomass_med_lead)
 # simple test plot
 plot(cap_postCollapse$biomass_med, cap_postCollapse$biomass_med_lead)
 abline(v = v90_post)
+

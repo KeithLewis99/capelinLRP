96fd8beb72662cb73d5b637db133d9e3b309c5b5
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-12-14 22:40:14 -0330

    Added SAR table and capcod figure, criteria, fixed roadmap outline, increased font sized on text only sections, fixed Bloss and B0 table, added helpfult text.

--- simpleLRP_calc.R
+++ simpleLRP_calc.R
@@ -553,6 +553,15 @@ plot(sr[9:37,]$biomass_tm2, sr[9:37,]$R, pch=16, col='steelblue')
 plot(segmented.fit_post, add=T)
 
 
+## "SSB" ----
+SSB <- read_csv("C:/Users/lewiske/Documents/capelin_LRP/IPM/data/SSB.csv")
+str(SSB)
+
+SSB$ssb_tm2 <- lag(SSB$I2mat + SSB$I3mat + SSB$I4mat, 2)
+
+SSB <- left_join(SSB, sr[1:35, c(1, 5)], by = "year")
+
+plot(SSB$ssb_tm2, SSB$R)
 
 # Historical LRP ----
 ## FROM MODELS: These will only be viable IF we get the IPM up and running
